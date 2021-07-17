6f9aaec12f638d07b20eed4893fc0bca2f84d7cb
jayiye27 <iyengarjayita@gmail.com>
2021-07-17 07:30:05 -0400

    Turtle Graphics

--- plum/TG_GreenRose.py
+++ plum/TG_GreenRose.py
@@ -1,10 +1,10 @@
 import turtle
 turtle.Pen()
-sam = turtle
-sam.speed(0)
-sam.color("green")
-sam.width(5)
+p = turtle
+p.speed(0)
+p.color("green")
+p.width(5)
 for i in range(100):
-    sam.forward(i*2)
-    sam.circle(i*2, 90)
-    sam.right(20)
+    p.forward(i*2)
+    p.circle(i*2, 90)
+    p.right(20)
