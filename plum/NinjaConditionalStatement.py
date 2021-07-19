625b0ab8e1b53162b277d2a89b2dffd21168b51e
jayiye27 <iyengarjayita@gmail.com>
2021-07-18 20:13:46 -0400

    Practicing Input and Conditional Statements

--- plum/NinjaConditionalStatement.py
+++ plum/NinjaConditionalStatement.py
@@ -1,10 +1,12 @@
 name = input("What's your name? ")
 print("Hello,", name)
+print("Imagine you are in a video game. Ninjas are your enemies. How many do you want to defeat?")
+print("Will the computer agree with your choice? Let's see!")
 
 ninjas = int(input("How many ninjas are you attacking? "))
 
 if ninjas > 50:
-    print("That's too many", name, "!!")
+    print("That's too many ninjas", name, "!!")
 elif ninjas > 20:
     print("Maybe we can take them down", name, ".")
 else:
