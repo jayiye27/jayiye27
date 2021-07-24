e62a55e1e627ac376e5466110f1ce102179ab8b6
jayiye27 <iyengarjayita@gmail.com>
2021-07-24 15:50:07 -0400

    Madlib using Python

---
+++ plum/Madlibs.py
@@ -0,0 +1,25 @@
+print("Hello there! This madlib contains 12 words. Enjoy!")
+adj1 = input("Adjective: ")
+propernoun1 = input("Proper Noun: ")
+adj0 = input("Color: ")
+verb1 = input("Verb: ")
+adj2 = input("Adjective: ")
+bodypart1 = input("Body Part: ")
+adj9 = input("Adjective: ")
+famous_person= input("Famous person: ")
+adj3 = input("Adjective: ")
+adj4 = input("Color: ")
+adj5 = input("Color: ")
+animal1 = input("Animal: ")
+noun1 = input("Noun: ")
+adverb1 = input("Adverb: ")
+
+
+madlib = f"It was just another {adj1} day. The sky was {adj2}, and the air was {adj3}. I grabbed my {noun1} with a determined look on  my face.\
+It is time, I thought. My {noun1} beside me, I {adverb1} {verb1} to the school field. {propernoun1} was next to me. We battled against the other team, the {animal1}'s.\
+No one could have know what happened next. The colors,  {adj4},  {adj5}, splattered everywhere!\
+Even on our {bodypart1}! Yuck! It was such a catashrophe.\
+Such a catashrophe that even {famous_person} stopped by! Yes the day was {adj9} and  {adj0}, but finally, in the end, everything was alright.\
+It is true, everyone  vanishished in to thin air, but my {noun1} was with me and that is all that matters."
+
+print (madlib)
