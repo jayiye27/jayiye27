a18741a97679b36ebd7d90ec7080f4ac2fa18463
jayiye27 <iyengarjayita@gmail.com>
2021-07-30 10:13:28 -0400

    Variables in Python

---
+++ plum/Variables.py
@@ -0,0 +1,18 @@
+#price = 10
+#price = 20
+#rating = 4.9
+#name = 'J'
+#is_published = False
+#print(price)
+#print("--------")
+
+print("A patient is entering the hospital.. What is his/her name and age?"
+      " Is the patient new or existing? Let's find out!")
+print("*****")
+print("Hi! My name is John Smith! I am 20 years old! Do I need to be registered?")
+name = 'John Smith'
+age = 20
+is_new = True
+
+print("Hi John Smith. You are a new patient, therefore you need to be registered. Thank you.")
+
