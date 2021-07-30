e24e0ae5adcabcb9d10fc397b9ff019b0637f905
jayiye27 <iyengarjayita@gmail.com>
2021-07-30 10:40:32 -0400

    Practicing Type Conversions in Python

---
+++ plum/TypeConversion.py
@@ -0,0 +1,11 @@
+birth_year = input('Birth year: ')
+year = input('Which year are you currently in: ')
+
+#print(type(birth_year))
+
+age = int(year) - int(birth_year)
+#print(type(age))
+
+print("Cool! You are, or are going to be")
+print(age)
+
