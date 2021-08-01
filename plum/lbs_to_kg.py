37ab981dcd29224d5d9afdd414c7c609004fad72
jayiye27 <iyengarjayita@gmail.com>
2021-08-01 13:25:38 -0400

    Practicing Type Conversions in Python

--- plum/lbs_to_kg.py
+++ plum/lbs_to_kg.py
@@ -1,4 +1,4 @@
-print("Hi! Welcome to the lbs to kgs converter")
+print("Hi! Welcome to the lbs to kgs converter!")
 weight_lbs = input('Weight (lbs): ')
 weight_kg = int(weight_lbs) * 0.45
 print("Okay! So your weight in kilograms is")
