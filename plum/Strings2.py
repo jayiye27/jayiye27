b7c623f8b5f1eb5e0a3fd104460fd79432246268
jayiye27 <iyengarjayita@gmail.com>
2021-08-04 09:11:58 -0400

    Practicing Strings in Python

---
+++ plum/Strings2.py
@@ -0,0 +1,9 @@
+course = 'Python for Beginners'
+print(course)
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find('Beginners'))
+print(course.replace('Beginners','Absolute Beginners'))
+print('Python'in course)
+print('python'in course)
