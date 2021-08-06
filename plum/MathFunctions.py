7869933801f049b8e849ce8ffc8260ff821af903
jayiye27 <iyengarjayita@gmail.com>
2021-08-06 09:37:00 -0400

    Math Functions in Python

---
+++ plum/MathFunctions.py
@@ -0,0 +1,15 @@
+import math
+
+x = 2.9
+
+#RoundNumber
+print(round(x))
+
+#AbsoluteFunction
+print(abs(-2.9))
+
+#MathModule
+#import math
+print(math.ceil(2.9))
+print(math.floor(-9))
+print(math.factorial(4))
