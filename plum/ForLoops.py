8d5088a7c76c4b38a6482aa16b5b9ffe1d621679
jayiye27 <iyengarjayita@gmail.com>
2021-08-14 07:23:42 -0400

    For Loops in Python

--- plum/ForLoops.py
+++ plum/ForLoops.py
@@ -1,2 +1,21 @@
 for item in 'Python':
     print(item)
+
+print("------")
+
+for item in ['Alice', 'John', 'Sarah']:
+    print(item)
+
+print("------")
+
+for item in [1, 2, 3, 4, 5, 6]:
+    print(item)
+
+#INSTEAD
+
+for item in range(10):
+    print(item)
+
+for item in range(5, 10, 2):
+    print(item)
+
