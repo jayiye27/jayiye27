e6a68bb3f5280d1a06d714a5c9e5af3da3ae53b0
jayiye27 <iyengarjayita@gmail.com>
2021-08-15 07:07:16 -0400

    Nested Loops in Python

---
+++ plum/NestedLoops.py
@@ -0,0 +1,24 @@
+for x in range(4):
+    for y in range(3):
+        print(f'({x},{y})')
+
+print('-------------------------')
+
+
+numbers = [5, 2, 5, 2, 2]
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output += 'x'
+    print(output)
+
+print("Look at this letter F ^^")
+
+print("--------------------------")
+numbers = [1,1,1,1,5]
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output += 'x'
+    print(output)
+print("Look at this letter L ^^")
