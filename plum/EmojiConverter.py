31594cabc210f7647d7a82c8d3130dd287461a99
jayiye27 <iyengarjayita@gmail.com>
2021-08-30 09:53:58 -0400

    Python

---
+++ plum/EmojiConverter.py
@@ -0,0 +1,13 @@
+message = input(">")
+words = message.split(' ')
+emojis = {
+    ":)": "🙂",
+    ":(": "☹",
+    ";)": "😉",
+    ":D":"😄"
+}
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+print(output)
+
