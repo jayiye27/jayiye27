c6460f9adfd69321c3a5fe18ecd69b3121fcd062
jayiye27 <iyengarjayita@gmail.com>
2021-12-01 16:32:41 -0500

    Python Guessing Game

--- plum/Project 2/Game.py
+++ plum/Project 2/Game.py
@@ -5,6 +5,11 @@ def guess(x):
     guess = 0
     while guess != random_number:
         guess = int(input(f"Guess a number between 1 and {x}:"))
-        print(guess)
+        if guess < random_number:
+            print("Sorry, guess again. Too low.")
+        elif guess > random_number:
+            print("Sorry, guess again. Too high.")
+
+    print(f"Yay, congrats. You have  guessed the number {random_number} correctly.")
 
 guess(10)
