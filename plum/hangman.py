a81f580834e6ce4ceb0b30dba0c595ce15c26a1a
jayiye27 <iyengarjayita@gmail.com>
2022-04-18 10:24:12 -0400

    Hangman

--- plum/hangman.py
+++ plum/hangman.py
@@ -13,16 +13,30 @@ def hangman():
     word = get_valid_word(words)
     word_letters = set(word) #letters in the word
     alphabet = set(string.ascii_uppercase)
-    used_letters = set()
+    used_letters = set() #what user has guessed
 
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
-    elif user_letter in used_letters:
-        print('You have already used that character. Please try again.')
-    else:
-        print('Invalid character. Please try again.')
+    #getting user input
+    while len(word_letters) > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print('You have used these letters ', ' '.join(used_letters))
 
+        #what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again.')
+
+        else:
+            print('Invalid character. Please try again.')
+
+    #gets here when len(word_letters) == 0
 user_input = input('Guess a letter:')
+hangman()
