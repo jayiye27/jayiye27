2707a400f6bf724c303cc42fc38b00e778a7bbf1
jayiye27 <iyengarjayita@gmail.com>
2023-07-10 12:43:43 -0400

    using stop index & step size with strings

--- plum/2023/strings.py
+++ plum/2023/strings.py
@@ -14,3 +14,38 @@ print(mystring[-2])
 
 #last letter of any string is [-1]
 
+#slicing
+
+mystring = 'abcdefghijk'
+print(mystring)
+
+print(mystring[2])
+
+#from c to the end
+
+print(mystring[2:])
+
+#print everything up to a particular index
+
+print(mystring[:3])
+
+#stop index (up to but not including)
+
+print(mystring[3:6])
+
+print(mystring[1:3])
+
+#print all
+print(mystring[::])
+
+#step size
+
+print(mystring[::2])
+print(mystring[::3])
+print(mystring[2:7:2])
+
+#reverse string
+
+print(mystring[::-1])
+mystring=mystring[::-1]
+print(mystring)
