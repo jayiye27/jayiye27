2da864c588c9bdf7c0f284546b846550af2b803f
jayiye27 <iyengarjayita@gmail.com>
2023-07-10 09:43:24 -0400

    indexing with strings

---
+++ plum/2023/strings.py
@@ -0,0 +1,16 @@
+mystring = "Hello World"
+
+#indexing characters
+
+print(mystring[0])
+
+print(mystring[8])
+
+print(mystring[9])
+
+#reverse indexing
+
+print(mystring[-2])
+
+#last letter of any string is [-1]
+
